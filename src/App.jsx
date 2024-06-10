f4d55604c763c5be7779ae6054baf7e76369e70a
Bob Oyier <boboyier92@gmail.com>
2024-06-10 23:21:11 +0300

    Do the stagger children animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,67 @@
+import { motion } from "framer-motion";
+
 export default function App() {
   return (
-    <h1 className="font-mono text-3xl font-bold text-red-400 underline">
-      Tailwind successfully added!
-    </h1>
+    <div className="flex flex-col gap-10 overflow-x-hidden">
+      {/* motion.section tells your application that this section will be animated */}
+      <motion.section
+        variants={{
+          hidden: { opacity: 0 }, // Make the section hidden
+          visible: {
+            opacity: 1, // Make the section visible
+            transition: {
+              // Add a transition to the animation
+              staggerChildren: 0.25, // Display each child 0.25 seconds after the previous
+            },
+          },
+        }}
+        initial="hidden" // Which of the variants do you want the page to load with
+        animate="visible" // What state should the page be in when the animation is done
+        className="grid grid-cols-3 gap-10 p-10"
+      >
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"
+        ></motion.div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"
+        ></motion.div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"
+        ></motion.div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"
+        ></motion.div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"
+        ></motion.div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"
+        ></motion.div>
+      </motion.section>
+    </div>
   );
 }
